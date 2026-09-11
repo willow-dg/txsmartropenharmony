ff833f0a9a73f4d67d23d9ce3e88e855042ca9f2
willow_dg <smshzt@outlook.com>
2026-09-11 05:01:22 +0300

    修复ambient_lighting_example.c中的WHITE颜色定义，确保RGB值正确设置

--- vendor/isoftstone/rk2206/samples/b9_rgb_led/ambient_lighting_example.c
+++ vendor/isoftstone/rk2206/samples/b9_rgb_led/ambient_lighting_example.c
@@ -36,7 +36,8 @@
 
 // 常见颜色的RGB值
 #define BLACK   RGB(0, 0, 0)
-#define WHITE   RGB(255, 255, 255)
+#define WHITE
+RGB(255, 255, 255)
 #define RED     RGB(255, 0, 0)
 #define GREEN   RGB(0, 255, 0)
 #define BLUE    RGB(0, 0, 255)
